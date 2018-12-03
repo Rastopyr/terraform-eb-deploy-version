7a14ffba00c41177b97172fe3dd0cd7993b4703e
Roman <rastopyr@gmail.com>
2018-12-03 20:38:27 +0200

    fix: remove extra variables

--- bin/build.sh
+++ bin/build.sh
@@ -13,10 +13,6 @@ version=$6
 source_bucket=$7
 source_key=$8
 
-AWS_ACCESS_KEY_ID=$access_key_id
-AWS_SECRET_ACCESS_KEY=$secret_key
-AWS_REGION=$aws_region
-
 AWS_ACCESS_KEY_ID=$access_key_id AWS_SECRET_ACCESS_KEY=$secret_key aws elasticbeanstalk create-application-version --application-name $application_name --version-label $version --source-bundle S3Bucket=$source_bucket,S3Key=$source_key --region $aws_region
 AWS_ACCESS_KEY_ID=$access_key_id AWS_SECRET_ACCESS_KEY=$secret_key aws elasticbeanstalk update-environment --environment-name $environment_name --version-label $version --region $aws_region
 
